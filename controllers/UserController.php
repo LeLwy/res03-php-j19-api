3a474a4dc1d0922c35af4023d4ff03b778c4cdf5
LeLwy <louis.chancioux@gmail.com>
2023-02-21 11:33:26 +0000

    MaJ controllers

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -10,37 +10,47 @@ class UserController extends AbstractController {
 
     public function getUsers()
     {
+        $users = $this->um->getAllUsers();
         // get all the users from the manager
-
+        
+        $this->render($users);
         // render
     }
 
     public function getUser(array $get)
     {
+        $user = $this->um->getUserById();
         // get the user from the manager
         // either by email or by id
-
+        
+        $this->render(['user' =>$user]);
         // render
     }
 
     public function createUser(array $post)
     {
+        $createdUser = $this->um->createUser();
         // create the user in the manager
 
+        $this->render(['createdUser' =>$createdUser]);
         // render the created user
     }
 
     public function updateUser(array $post)
     {
+        $updatedUser = $this->um->updateUser();
         // update the user in the manager
 
+        $this->render(['updatedUser' =>$updatedUser]);
         // render the updated user
     }
 
     public function deleteUser(array $post)
     {
+        $deletedUser = $this->um->deleteUser();
         // delete the user in the manager
 
+        $this->render($deletedUser);
         // render the list of all users
     }
 }
